de8c31c7c54188431f810ade673a5a93122c802a
unknown <dhrisher@hotmail.co.uk>
2021-01-20 21:35:44 +0000

    Adding forgotten password functionality and registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     # post views
     #path('login/', views.user_login, name='login'),
+    path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
