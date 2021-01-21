423d7507b2070dfb70f885997bebc004af1c0a86
unknown <dhrisher@hotmail.co.uk>
2021-01-21 21:16:15 +0000

    add user registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -26,6 +26,7 @@ def user_login(request):
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
+
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
@@ -36,7 +37,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # save the user object
             new_user.save()
-            return render(request, 'account/registration_done.html', {'new_user': new_user})
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
